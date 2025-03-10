08ff95d86aa81bf7d694117cdc5a7c7def7dbcba
Yannis4444 <yannis44@web.de>
2025-03-10 23:16:35 +0100

    local buffers for consumers, distributing to consumers on produce

--- app/api/endpoints/resources.py
+++ app/api/endpoints/resources.py
@@ -4,7 +4,6 @@ from fastapi import APIRouter, HTTPException
 
 from app.api.schemas.resource import ProducedResource, ConsumedResource
 from app.services.prosumer import get_consumer, get_producer
-from app.services.resource import get_resource
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
@@ -12,19 +11,13 @@ router = APIRouter()
 
 @router.post("/produce", response_model=ProducedResource)
 async def produce(producer_id: str, resource_id: str, amount: int):
-    producer = await get_producer(producer_id)
+    producer = await get_producer(producer_id, resource_id=resource_id)
 
     if producer is None:
         logger.error(f"Producer '{producer_id}' not found")
         raise HTTPException(status_code=404, detail="Producer not found")
 
-    resource = await get_resource(resource_id)
-
-    if resource is None:
-        logger.error(f"Resource '{resource_id}' not found")
-        raise HTTPException(status_code=404, detail="Resource not found")
-
-    produced_amount = await producer.produce(resource, amount)
+    produced_amount = await producer.produce(amount)
 
     return ProducedResource(
         amount=produced_amount,
@@ -33,19 +26,13 @@ async def produce(producer_id: str, resource_id: str, amount: int):
 
 @router.post("/consume", response_model=ConsumedResource)
 async def consume(consumer_id: str, resource_id: str, amount: int):
-    consumer = await get_consumer(consumer_id)
+    consumer = await get_consumer(consumer_id, resource_id)
 
     if consumer is None:
         logger.error(f"Consumer '{consumer_id}' not found")
         raise HTTPException(status_code=404, detail="Consumer not found")
 
-    resource = await get_resource(resource_id)
-
-    if resource is None:
-        logger.error(f"Resource '{resource_id}' not found")
-        raise HTTPException(status_code=404, detail="Resource not found")
-
-    consumed_amount = await consumer.consume(resource, amount)
+    consumed_amount = await consumer.consume(amount)
 
     return ProducedResource(
         amount=consumed_amount,
--- app/services/prosumer.py
+++ app/services/prosumer.py
@@ -1,14 +1,167 @@
 import asyncio
 import logging
 
-from app.services.resource import get_resource, Resource
-
 logger = logging.getLogger(__name__)
+
+
+class Resource:
+    """
+    Representation of a resource.
+    Only one instance should exist for each type of resource
+    """
+
+    def __init__(self, resource_id: str, buffer_limit: int, buffer: int = 0):
+        """
+        Creates a new resource.
+
+        :param resource_id: A resource identifier
+        :param buffer_limit: The maximum amount of the resource that can be stored at once
+        :param buffer: The starting buffer of the resource
+        """
+
+        self._id = resource_id
+        self._buffer_limit = buffer_limit
+
+        self._buffer = buffer
+        self._buffer_lock = asyncio.Lock()
+
+        self._consumers: set[Consumer] = set()
+
+    def __repr__(self) -> str:
+        return f"Resource(resource_id={self._id}, buffer_limit={self._buffer_limit}, buffer={self._buffer})"
+
+    def __str__(self) -> str:
+        return self._id
+
+    @property
+    def id(self) -> str:
+        """
+        The identifier for the resource
+
+        :return: The id
+        """
+
+        return self._id
+
+    def add_consumer(self, consumer: 'Consumer'):
+        """
+        Adds the given consumer to be considered when producing
+
+        :param consumer: The consumer to add
+        """
+
+        # TODO: option to remove
+
+        self._consumers.add(consumer)
+
+    async def produce(self, amount: int) -> int:
+        """
+        Adds an amount of the resource and returns how much was actually stored.
+
+        When the storage buffer limit is reached, the return value will be less than the set amount.
+
+        :param amount: The amount of the resource to be produced.
+        :return: The amount that was actually stored.
+        """
+
+        # first put the produced amount into the buffers of the consumers
+        consumers = list(self._consumers)
+        while amount > 0 and consumers:
+            amount_per_consumer = amount // len(_consumers)
+            n_consumers_additional_resource = amount % len(_consumers)
+            next_consumers = []
+
+            for i, consumer in enumerate(consumers):
+                # TODO: rotate distribution of remainder - not just always the first ones
+                consumer_amount = amount_per_consumer + (1 if i < n_consumers_additional_resource else 0)
+                actual_amount = await consumer.add_to_buffer(consumer_amount)
+
+                if actual_amount == consumer_amount:
+                    # The consumer might still take more of the resource if anything remains
+                    next_consumers.append(consumer)
+
+                amount -= actual_amount
+
+            consumers = next_consumers
+
+        if amount > 0:
+            async with self._buffer_lock:
+                actual_amount = min(amount, self._buffer_limit - self._buffer)
+                self._buffer += actual_amount
+
+        logging.info(repr(self))
+        logging.info(self._consumers)
+
+        return actual_amount
+
+    async def consume(self, amount: int) -> int:
+        """
+        Removes an amount of the resource and returns how much was actually taken.
+
+        When the storage is empty, the return value will be less than the set amount.
+
+        :param amount: The amount of the resource to be consumed.
+        :return: The amount that was actually consumed.
+        """
+
+        async with self._buffer_lock:
+            actual_amount = min(amount, self._buffer)
+            self._buffer -= actual_amount
+            return actual_amount
+
+
+_resource_creation_lock = asyncio.Lock()
+_resources: dict[str, Resource] = {}
+
+
+async def get_resource(resource_id) -> Resource:
+    """
+    Gets the resource with the given id.
+    Should always be used to get any resource
+
+    :param resource_id: The identifier for the resource to get
+    :return: The resource
+    """
+    if resource_id not in _resources:
+        async with _resource_creation_lock:
+            if resource_id not in _resources:
+                # TODO: actual buffer limit
+                _resources[resource_id] = Resource(resource_id, 100)
+
+    return _resources.get(resource_id)
+
 
 class Prosumer:
     """
     Abstract base class for both Producer and Consumer.
     """
+
+    def __init__(self, prosumer_id: str, resource: Resource):
+        """
+        Creates a new consumer
+
+        :param prosumer_id: An identifier for the consumer
+        :param resource: The resource this consumer consumes.
+        """
+
+        self._id = prosumer_id
+        self._resource = resource
+
+    def __repr__(self):
+        return f"Prosumer(id={self._id}, resource={self._resource})"
+
+    def __str__(self):
+        return self._id
+
+    @property
+    def resource(self) -> Resource:
+        """
+        The resource the prosumer is handling
+
+        :return: The resource
+        """
+
+        return self._resource
 
 
 class Producer(Prosumer):
@@ -16,38 +169,32 @@ class Producer(Prosumer):
     Defines the interface for a Producer.
     """
 
-    def __init__(self, producer_id: str):
+    def __init__(self, producer_id: str, resource: Resource):
         """
         Creates a new producer
 
         :param producer_id: An identifier for the producer
+        :param resource: The resource this consumer consumes.
         """
 
-        self._producer_id = producer_id
+        super().__init__(producer_id, resource)
 
     def __repr__(self):
-        return f"Producer(producer_id={self._producer_id})"
+        return f"Producer(id={self._id}, resource={self._resource})"
 
-    def __str__(self):
-        return self._producer_id
-
-    async def produce(self, resource: Resource | str, amount: int) -> int:
+    async def produce(self, amount: int) -> int:
         """
-        Adds an amount of the resource and returns how much was actually stored.
+        Adds an amount of the handled resource and returns how much was actually stored.
 
         When the storage limit is reached, the return value will be less than the set amount.
 
-        :param resource: The type of resource to be produced, represented as a string.
         :param amount: The quantity of the resource to be produced, represented as an integer.
         :return: The amount that was actually stored.
         """
 
-        if isinstance(resource, str):
-            resource = await get_resource(resource)
+        actual_amount = await self.resource.produce(amount)
 
-        actual_amount = await resource.produce(amount)
-
-        logger.info(f"{self} produced {actual_amount}/{amount} of {resource}")
+        logger.info(f"{self} produced {actual_amount}/{amount} of {self.resource}")
 
         return actual_amount
 
@@ -57,38 +204,66 @@ class Consumer(Prosumer):
     Defines the interface for a Consumer.
     """
 
-    def __init__(self, consumer_id: str):
+    def __init__(self, consumer_id: str, resource: Resource, buffer_limit: int, buffer: int = 0):
         """
         Creates a new consumer
 
         :param consumer_id: An identifier for the consumer
+        :param resource: The resource this consumer consumes.
+        :param buffer_limit: The maximum amount of the resource that can be stored at once
+        :param buffer: The starting buffer of the resource
         """
 
-        self._consumer_id = consumer_id
+        super().__init__(consumer_id, resource)
+
+        resource.add_consumer(self)
+
+        self._buffer_limit = buffer_limit
+        self._buffer = buffer
+        self._buffer_lock = asyncio.Lock()
 
     def __repr__(self):
-        return f"Consumer(consumer_id={self._consumer_id})"
+        return f"Consumer(id={self._id}, resource={self._resource}, buffer_limit={self._buffer_limit}, buffer={self._buffer})"
 
     def __str__(self):
-        return self._consumer_id
+        return self._id
 
-    async def consume(self, resource: Resource | str, amount: int) -> int:
+    async def add_to_buffer(self, amount: int) -> int:
         """
-        Adds an amount of the resource and returns how much was actually stored.
+        Adds the given amount to the local consumer buffer
 
         When the storage limit is reached, the return value will be less than the set amount.
 
-        :param resource: The type of resource to be consumed, represented as a string.
+        :param amount: The amount to add
+        :return: The amount actually added
+        """
+
+        async with self._buffer_lock:
+            actual_amount = min(amount, self._buffer_limit - self._buffer)
+            self._buffer += actual_amount
+            return actual_amount
+
+    async def consume(self, amount: int) -> int:
+        """
+        Adds an amount of the handled resource and returns how much was actually stored.
+
+        When the buffer is emptied, the return value will be less than the set amount.
+
         :param amount: The quantity of the resource to be consumed, represented as an integer.
         :return: The amount that was actually stored.
         """
 
-        if isinstance(resource, str):
-            resource = await get_resource(resource)
+        # first try to take from the global resource buffer
+        actual_amount = await self._resource.consume(amount)
 
-        actual_amount = await resource.consume(amount)
+        # take rest from consumer buffer
+        if actual_amount < amount:
+            async with self._buffer_lock:
+                additional_amount = min(amount - actual_amount, self._buffer)
+                actual_amount += additional_amount
+                self._buffer -= additional_amount
 
-        logger.info(f"{self} consumed {actual_amount}/{amount} of {resource}")
+        logger.info(f"{self} consumed {actual_amount}/{amount} of {self.resource}")
 
         return actual_amount
 
@@ -97,21 +272,21 @@ _consumer_creation_lock = asyncio.Lock()
 _consumers: dict[str, Consumer] = {}
 
 
-async def get_consumer(consumer_id) -> Consumer:
+async def get_consumer(consumer_id: str, resource_id: str) -> Consumer:
     """
     Gets the consumer with the given id.
     Should always be used to get any consumer
 
     :param consumer_id: The identifier for the consumer to get
+    :param resource_id: The identifier for the handled resource
     :return: The consumer
     """
-
-    # TODO: later not just create one but use the config?
 
     if consumer_id not in _consumers:
         async with _consumer_creation_lock:
             if consumer_id not in _consumers:
-                _consumers[consumer_id] = Consumer(consumer_id)
+                # TODO: actual buffer settings
+                _consumers[consumer_id] = Consumer(consumer_id, await get_resource(resource_id), 100)
 
     return _consumers.get(consumer_id)
 
@@ -120,20 +295,19 @@ _producer_creation_lock = asyncio.Lock()
 _producers: dict[str, Producer] = {}
 
 
-async def get_producer(producer_id) -> Producer:
+async def get_producer(producer_id: str, resource_id: str) -> Producer:
     """
     Gets the producer with the given id.
     Should always be used to get any producer
 
     :param producer_id: The identifier for the producer to get
+    :param resource_id: The identifier for the handled resource
     :return: The producer
     """
-
-    # TODO: later not just create one but use the config?
 
     if producer_id not in _producers:
         async with _producer_creation_lock:
             if producer_id not in _producers:
-                _producers[producer_id] = Producer(producer_id)
+                _producers[producer_id] = Producer(producer_id, await get_resource(resource_id))
 
     return _producers.get(producer_id)
